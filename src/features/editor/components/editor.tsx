a8113cfbd5101a1e15f3b5b8bd588a67f0786fab
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-22 23:27:06 +0200

    added canvasref and worspaceref to editor feature

--- src/features/editor/components/editor.tsx
+++ src/features/editor/components/editor.tsx
@@ -1,18 +1,21 @@
 "use client"
 
-import { useEffect } from "react"
+import { useEffect, useRef } from "react"
 import { useEditor } from "../hooks/useEditor"
 
 export const Editor = () => {
     const {init} = useEditor()
+
+    const canvasRef = useRef(null)
+    const workspaceRef = useRef(null)
 
     useEffect(() =>{
         init()
     },[])
 
     return (
-        <div>
-            Editor components
+        <div ref={workspaceRef}>
+            <canvas ref={canvasRef}/>
         </div>
     )
 }  
