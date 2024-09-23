56b71df27daf4e2a139a31285428c24542a887c8
ericgro2000 <eric.gro.2000@gmail.com>
2024-09-23 23:15:50 +0200

    added main sheet in editor, black rect for test purpose and customized its controles

--- src/features/editor/hooks/useEditor.ts
+++ src/features/editor/hooks/useEditor.ts
@@ -1,3 +1,4 @@
+import {fabric} from "fabric";
 import { useCallback } from "react"
 
 export const useEditor = () => {
@@ -8,8 +9,43 @@ export const useEditor = () => {
         initialCanvas: any;
         initialContainer:HTMLDivElement
     }) => {
+        fabric.Object.prototype.set({
+            cornerColor: "#FFF",
+            cornerStyle: "circle",
+            borderColor: "#3b82f6",
+            borderScaleFactor: 1.5,
+            transparentCorners: false,
+            borderOpacityWhenMoving: 1,
+            cornerStrokeColor: "#3b82f6",
+          });
+          
+        const initialWorkspace = new fabric.Rect({
+            // width: initialWidth.current,
+            // height: initialHeight.current,
+            width: 900,
+            height: 1200,
+            name: "clip",
+            fill: "white",
+            selectable: false,
+            hasControls: false,
+            shadow: new fabric.Shadow({
+              color: "rgba(0,0,0,0.8)",
+              blur: 5,
+            }),
+          });
+
+          initialCanvas.add(initialWorkspace);
+          initialCanvas.centerObject(initialWorkspace);
+          initialCanvas.clipPath = initialWorkspace;
+    
         initialCanvas.setWidth(initialContainer.offsetWidth);
         initialCanvas.setHeight(initialContainer.offsetHeight);
+
+        const rect = new fabric.Rect(
+            {height:100,width:100,fill:"black"}
+          )
+          initialCanvas.add(rect);
+          initialCanvas.centerObject(rect);
     },[])
 
     return {init}
