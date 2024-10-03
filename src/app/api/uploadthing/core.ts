d8b15924c1b985a6ff6020267a52ec190b1fb9f1
ericgro2000 <eric.gro.2000@gmail.com>
2024-10-04 00:32:10 +0200

    added upload images functionality

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,7 +1,7 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
-import { auth } from "@/auth";
+// import { auth } from "@/auth";
 
 const f = createUploadthing();
 
