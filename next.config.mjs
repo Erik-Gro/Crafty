2f103e1635a3abe905d9dbb9d323038a733a6218
ericgro2000 <eric.gro.2000@gmail.com>
2024-10-04 20:28:37 +0200

    removed comment

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,6 @@ const nextConfig = {
         protocol: "https",
         hostname: "images.unsplash.com",
       },
-      // might need it when shipping to prod
       {
         protocol: "https",
         hostname: "utfs.io",
