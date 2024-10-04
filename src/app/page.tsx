42774b899d661b48fb1440e92b5d3156c9ceaabd
Eric <eric.gro.2000@gmail.com>
2024-10-05 01:50:56 +0200

    added saving user to db after auth through github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,3 +6,13 @@ export default async function Home() {
   const session = await auth();
   return <div>{JSON.stringify(session)}</div>;
 }
+
+// import { auth } from "@/auth";
+// import { Button } from "@/components/ui/button";
+// import { protectServer } from "@/features/auth/util";
+// import Image from "next/image";
+
+// export default async function Home() {
+//   protectServer();
+//   return <div>you are logged in</div>;
+// }
--- src/auth.ts
+++ src/auth.ts
@@ -1,6 +1,15 @@
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
+import { db } from "./db/drizzle";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: DrizzleAdapter(db),
   providers: [GitHub],
 });
+
+// import NextAuth from "next-auth";
+
+// import authConfig from "@/auth.config";
+
+// export const { handlers, signIn, signOut, auth } = NextAuth(authConfig);
