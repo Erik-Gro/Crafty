24ccf73696d16d245729c1d18356a2f54ac2eace
ericgro2000 <eric.gro.2000@gmail.com>
2024-10-04 21:54:08 +0200

    testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
+import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div>
-      <Button size="sm" variant="destructive">
-        Click me
-      </Button>
-    </div>
-  );
+export default async function Home() {
+  const session = await auth();
+  return <div>{JSON.stringify(session)}</div>;
 }
