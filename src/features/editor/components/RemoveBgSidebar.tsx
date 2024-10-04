b9e6a955e7b61deaa945d348046f69d072e1b2a4
ericgro2000 <eric.gro.2000@gmail.com>
2024-10-04 03:38:19 +0200

    fixed error

--- src/features/editor/components/RemoveBgSidebar.tsx
+++ src/features/editor/components/RemoveBgSidebar.tsx
@@ -5,22 +5,22 @@ import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Editor } from "../types/editor";
 import { ActiveTool } from "../types/activeTools";
-import { ToolSidebarClose } from "@/components/ui/ToolSidebarClose";
-import { ToolSidebarHeader } from "@/components/ui/ToolSidebarHeader";
 import { useRemoveBg } from "../ai/api/useRemoveBg";
+import { ToolSidebarHeader } from "@/components/ui/ToolSidebarHeader";
+import { ToolSidebarClose } from "@/components/ui/ToolSidebarClose";
 
 interface RemoveBgSidebarProps {
   editor: Editor | undefined;
   activeTool: ActiveTool;
   onChangeActiveTool: (tool: ActiveTool) => void;
-};
+}
 
 export const RemoveBgSidebar = ({
   editor,
   activeTool,
   onChangeActiveTool,
 }: RemoveBgSidebarProps) => {
-//   const { shouldBlock, triggerPaywall } = usePaywall();
+  // const { shouldBlock, triggerPaywall } = usePaywall();
   const mutation = useRemoveBg();
 
   const selectedObject = editor?.selectedObjects[0];
@@ -32,26 +32,29 @@ export const RemoveBgSidebar = ({
     onChangeActiveTool("select");
   };
 
-//   const onClick = () => {
-//     if (shouldBlock) {
-//       triggerPaywall();
-//       return;
-//     }
+  const onClick = () => {
+    // if (shouldBlock) {
+    //   triggerPaywall();
+    //   return;
+    // }
 
-    mutation.mutate({
-      image: imageSrc,
-    }, {
-      onSuccess: ({ data }) => {
-        editor?.addImage(data);
+    mutation.mutate(
+      {
+        image: imageSrc,
       },
-    });
+      {
+        onSuccess: ({ data }) => {
+          editor?.addImage(data);
+        },
+      }
+    );
   };
 
   return (
     <aside
       className={cn(
         "bg-white relative border-r z-[40] w-[360px] h-full flex flex-col",
-        activeTool === "remove-bg" ? "visible" : "hidden",
+        activeTool === "remove-bg" ? "visible" : "hidden"
       )}
     >
       <ToolSidebarHeader
@@ -69,16 +72,13 @@ export const RemoveBgSidebar = ({
       {imageSrc && (
         <ScrollArea>
           <div className="p-4 space-y-4">
-            <div className={cn(
-              "relative aspect-square rounded-md overflow-hidden transition bg-muted",
-              mutation.isPending && "opacity-50",
-            )}>
-              <Image
-                src={imageSrc}
-                fill
-                alt="Image"
-                className="object-cover"
-              />
+            <div
+              className={cn(
+                "relative aspect-square rounded-md overflow-hidden transition bg-muted",
+                mutation.isPending && "opacity-50"
+              )}
+            >
+              <Image src={imageSrc} fill alt="Image" className="object-cover" />
             </div>
             <Button
               disabled={mutation.isPending}
