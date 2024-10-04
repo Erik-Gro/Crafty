6a4ef9994aed646b18aaef8ada475176eba48e58
ericgro2000 <eric.gro.2000@gmail.com>
2024-10-04 02:35:44 +0200

    added ai generating of image functionality

--- src/features/editor/components/aiSidebar.tsx
+++ src/features/editor/components/aiSidebar.tsx
@@ -8,6 +8,7 @@ import { Editor } from "../types/editor";
 import { ActiveTool } from "../types/activeTools";
 import { ToolSidebarHeader } from "@/components/ui/ToolSidebarHeader";
 import { ToolSidebarClose } from "@/components/ui/ToolSidebarClose";
+import { useGenerateImage } from "../ai/api/useGenerateImage";
 
 interface AiSidebarProps {
   editor: Editor | undefined;
