943c339288f9aad3312a8c17e738d6e5db19b8a2
Eric <eric.gro.2000@gmail.com>
2024-10-05 19:04:07 +0200

    protected non auth apis

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,14 +1,26 @@
 import { Context, Hono } from "hono";
 import { handle } from "hono/vercel";
+import { AuthConfig, initAuthConfig } from "@hono/auth-js";
 
 import images from "./images";
 import ai from "./ai";
 import users from "./users";
 
+import authConfig from "@/auth.config";
+
 // Revert to "nodejs" if planning on running on the nodejs
-export const runtime = "node.js";
+export const runtime = "nodejs";
+
+function getAuthConfig(c: Context): AuthConfig {
+  return {
+    secret: c.env.AUTH_SECRET,
+    ...authConfig,
+  };
+}
 
 const app = new Hono().basePath("/api");
+
+app.use("*", initAuthConfig(getAuthConfig));
 
 const routes = app
   .route("/ai", ai)
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,19 +1,18 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 
-// import { auth } from "@/auth";
+import { auth } from "@/auth";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
   imageUploader: f({ image: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
-      // const session = await auth();
+      const session = await auth();
 
-      // if (!session) throw new UploadThingError("Unauthorized");
+      if (!session) throw new UploadThingError("Unauthorized");
 
-      // return { userId: session.user?.id };
-      return {};
+      return { userId: session.user?.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
       return { url: file.url };
