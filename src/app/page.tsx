2637f54ab06ba43d2606c82afc97453c9d59bf9f
Eric <eric.gro.2000@gmail.com>
2024-10-05 02:18:09 +0200

    saving(too tired)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
+import { protectServer } from "@/features/auth/util";
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const session = await auth();
-  return <div>{JSON.stringify(session)}</div>;
+  await protectServer();
+  return <div>you are logged in</div>;
 }
-
-// import { auth } from "@/auth";
-// import { Button } from "@/components/ui/button";
-// import { protectServer } from "@/features/auth/util";
-// import Image from "next/image";
-
-// export default async function Home() {
-//   protectServer();
-//   return <div>you are logged in</div>;
-// }
