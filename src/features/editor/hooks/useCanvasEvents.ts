bb88de922836ea0651c6ac450302e6b9dcfa1270
Eric <eric.gro.2000@gmail.com>
2024-10-27 00:35:19 +0200

    removed unneeded comments

--- src/features/editor/hooks/useCanvasEvents.ts
+++ src/features/editor/hooks/useCanvasEvents.ts
@@ -18,20 +18,7 @@ export const useCanvasEvents = ({
     if (canvas) {
       canvas.on("object:added", () => save());
       canvas.on("object:removed", () => save());
-      canvas.on("object:modified", (e) => {
-        const obj = e.target;
-
-        // Check if obj is defined
-        if (!obj) return;
-
-        // Use optional chaining and default values for width and scale
-        const currentWidth = (obj.width ?? 0) * (obj.scaleX ?? 1);
-        const currentHeight = (obj.height ?? 0) * (obj.scaleY ?? 1);
-
-        // console.log(
-        //   `Current Width: ${currentWidth}, Current Height: ${currentHeight}`
-        // );
-
+      canvas.on("object:modified", () => {
         save();
       });
 
@@ -100,46 +87,8 @@ export const useCanvasEvents = ({
         canvas.off("selection:created");
         canvas.off("selection:updated");
         canvas.off("selection:cleared");
-        canvas.off("object:moving"); // Clean up the object:moving event
+        canvas.off("object:moving");
       }
     };
   }, [save, canvas, clearSelectionCallback, setSelectedObjects]);
 };
-
-// canvas.on("object:moving", function (event) {
-//   const obj = event.target as fabric.Object | undefined;
-
-//   // Check if obj is defined and has the necessary properties
-//   if (!obj || typeof obj.left === 'undefined' || typeof obj.top === 'undefined') {
-//     return; // Exit if obj is not valid
-//   }
-
-//   const threshold = 20; // Snapping threshold in pixels
-
-//   // Find the clipping rectangle (clipPath)
-//   const clipPath = canvas.clipPath as fabric.Rect | undefined;
-
-//   // Ensure clipPath is valid
-//   if (!clipPath) return;
-
-//   const workspaceLeft = clipPath.left || 0;
-//   const workspaceTop = clipPath.top || 0;
-//   const workspaceWidth = clipPath.width || 0;
-//   const workspaceHeight = clipPath.height || 0;
-
-//   // Snap to edges of the clipping area
-//   if (obj.left < workspaceLeft + threshold) {
-//     obj.left = workspaceLeft; // Snap to left edge
-//   } else if (obj.left > workspaceLeft + workspaceWidth - (obj.width || 0) - threshold) {
-//     obj.left = workspaceLeft + workspaceWidth - (obj.width || 0); // Snap to right edge
-//   }
-
-//   if (obj.top < workspaceTop + threshold) {
-//     obj.top = workspaceTop; // Snap to top edge
-//   } else if (obj.top > workspaceTop + workspaceHeight - (obj.height || 0) - threshold) {
-//     obj.top = workspaceTop + workspaceHeight - (obj.height || 0); // Snap to bottom edge
-//   }
-
-//   // Update the object's position
-//   obj.setCoords();
-// });
